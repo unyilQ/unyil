121f03f6b2fcb65a6f64070139b10c6c6d627aa3
unyilQ <89531900+unyilQ@users.noreply.github.com>
2022-03-04 22:22:56 -0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Dian Ganteng" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user admin @Dian/add >nul
+net user admin @DianGanteng/add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -16,6 +16,6 @@ echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: admin
-echo Password: @Dian
+echo Password: @DianGanteng
 echo Silahkan Login Ke RDP Anda Sekarang
 ping -n 10 127.0.0.1 >nul
